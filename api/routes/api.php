6f6ca0387e2e9f9f66d3e16bcfaca94a76efdb89
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2020-04-04 14:35:57 +0200

    Fix api routes file

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,48 +14,15 @@ use Illuminate\Support\Facades\Route;
   |
 */
 
-Route::get('accounts/{id}', function ($id) {
-    $account = DB::table('accounts')
-             ->whereRaw("id=$id")
-             ->get();
+Route::get('/currencies', ['as' => 'currencies.list', 'uses' => 'CurrenciesController@index']);
 
-    return $account;
-});
+Route::prefix('accounts')->group(function () {
+    Route::get('/', ['as' => 'accounts.list', 'uses' => 'AccountController@index']);
+    Route::post('/', ['as' => 'accounts.store', 'uses' => 'AccountController@store']);
+    Route::get('/{id}', ['as' => 'accounts.show', 'uses' => 'AccountController@show']);
+    Route::put('/{id}', ['as' => 'accounts.update', 'uses' => 'AccountController@update']);
+    Route::delete('/{id}', ['as' => 'accounts.destroy', 'uses' => 'AccountController@destroy']);
 
-Route::get('accounts/{id}/transactions', function ($id) {
-    $account = DB::table('transactions')
-             ->whereRaw("`from`=$id OR `to`=$id")
-             ->get();
-
-    return $account;
-});
-
-Route::post('accounts/{id}/transactions', function (Request $request, $id) {
-    $to = $request->input('to');
-    $amount = $request->input('amount');
-    $details = $request->input('details');
-
-    $account = DB::table('accounts')
-             ->whereRaw("id=$id")
-             ->update(['balance' => DB::raw('balance-' . $amount)]);
-
-    $account = DB::table('accounts')
-             ->whereRaw("id=$to")
-             ->update(['balance' => DB::raw('balance+' . $amount)]);
-
-    DB::table('transactions')->insert(
-        [
-            'from' => $id,
-            'to' => $to,
-            'amount' => $amount,
-            'details' => $details
-        ]
-    );
-});
-
-Route::get('currencies', function () {
-    $account = DB::table('currencies')
-              ->get();
-
-    return $account;
+    Route::get('/{id}/transactions', ['as' => 'account.transactions', 'uses' => 'AccountController@getTransactions']);
+    Route::post('/{id}/transactions', ['as' => 'account.transactions', 'uses' => 'AccountController@makeTransaction']);
 });
