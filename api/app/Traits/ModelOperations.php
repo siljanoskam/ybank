b874b4f4c86760ab2d2b6c407030095e29db279e
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2020-04-04 14:10:38 +0200

    Add utility functions for model operations

---
+++ api/app/Traits/ModelOperations.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait ModelOperations
+{
+    protected function getAllModelItems(Model $model)
+    {
+        return $model->all();
+    }
+
+    protected function storeModelItem(Model $model, $data)
+    {
+        return $model->create($data);
+    }
+
+    protected function showModelItem(Model $model, $id)
+    {
+        return $model->findOrFail($id);
+    }
+
+    protected function updateModelItem(Model $model, $id, $data)
+    {
+        $item = $this->showModelItem($model, $id);
+
+        $item->update($data);
+
+        return $item;
+    }
+
+    protected function destroyModelItem(Model $model, $id)
+    {
+        $item = $this->showModelItem($model, $id);
+
+        return $item->delete();
+    }
+}
