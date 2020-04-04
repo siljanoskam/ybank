1e50862d2772497939fdafa5a91ad199503c4e18
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2020-04-04 14:44:16 +0200

    Add BaseRequest class

---
+++ api/app/Http/Requests/BaseRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Response\Std400Response;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class BaseRequest extends FormRequest
+{
+    /**
+     * Set a validation error response here
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $httpStatusCode = Response::HTTP_UNPROCESSABLE_ENTITY;
+        $response = new Std400Response();
+        $response->setStatusCode($httpStatusCode);
+        $response->setErrors([$validator->errors()]);
+
+        throw new HttpResponseException(
+            response()->json($response, Response::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
