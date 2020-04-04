2a34c5872a9b60deef4575318a6688ac1fc78324
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2020-04-04 14:42:43 +0200

    Add Currencies list getting functionality

---
+++ api/app/Http/Controllers/CurrencyController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\AdminPanel;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\Accounts;
+use App\Repositories\Currencies;
+use App\Traits\ResponseUtils;
+use Illuminate\Http\JsonResponse;
+
+class CurrencyController extends Controller
+{
+    use ResponseUtils;
+
+    protected $currenciesRepository;
+
+    public function __construct()
+    {
+        $this->currenciesRepository = new Currencies();
+    }
+
+    /**
+     * Returns a list of all currencies
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $currenciesFetch =
+            $this
+                ->currenciesRepository
+                ->all();
+
+        if ($currenciesFetch->hasError()) {
+            return $this->errorResponse(
+                $currenciesFetch->getErrorMessage()
+            );
+        }
+
+        return $this->successResponse($currenciesFetch->getItems());
+    }
+}
---
+++ api/app/Repositories/Currencies.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Currency;
+use App\Http\Resources\Currency as CurrencyResource;
+use App\Traits\ModelOperations;
+use Illuminate\Support\Facades\Log;
+
+class Currencies extends Repository
+{
+    use ModelOperations;
+
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new Currency();
+
+    }
+
+    public function all(): Repository
+    {
+        try {
+            $currencies = $this->getAllModelItems($this->model);
+            $currenciesList = CurrencyResource::collection($currencies);
+        } catch (\Exception $exception) {
+            Log::error(
+                'Something went wrong while getting the currencies from the database',
+                [
+                    'message' => $exception->getMessage()
+                ]
+            );
+            $error = true;
+            $errorMessage = $exception->getMessage();
+        }
+
+        return (new Repository())
+            ->setItems($currenciesList ?? [])
+            ->setError($error ?? false)
+            ->setErrorMessage($errorMessage ?? '');
+    }
+}
