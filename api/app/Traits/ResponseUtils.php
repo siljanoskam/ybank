49cc857d7b7102adb5b972137760fd5827855c67
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2020-04-04 14:11:04 +0200

    Add utility functions for responses

---
+++ api/app/Traits/ResponseUtils.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Traits;
+
+use App\Http\Response\Std200ListResponse;
+use App\Http\Response\Std200Response;
+use App\Http\Response\Std400Response;
+use App\Http\Response\Std500Response;
+use App\Http\Response\StdEmptyResponse;
+use App\Http\Response\StdEmptyResponseInterface;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response as HttpStatusCode;
+
+trait ResponseUtils
+{
+    /**
+     * Returns a JSON response with a status code based on the provided response object
+     *
+     * @param StdEmptyResponseInterface $response An object that implements
+     * the StdEmptyResponseInterface
+     *
+     * @return JsonResponse
+     */
+    private function response(StdEmptyResponseInterface $response): JsonResponse
+    {
+        return response()->json($response, $response->getStatusCode());
+    }
+
+    /**
+     * A helper method that returns error responses for requests that have been
+     * validated but failed at another step
+     *
+     * @param string $error
+     * @param int $type The type of error response (400 or 500).
+     *
+     * @return JsonResponse
+     */
+    public function errorResponse(string $error, int $type = 500): JsonResponse
+    {
+        $response = $type === 400 ? new Std400Response() : new Std500Response();
+        $response->addError($error);
+
+        return $this->response($response);
+    }
+
+    /**
+     * A helper method for success that returns lists
+     *
+     * @param array $data
+     * @return JsonResponse
+     */
+    public function successListResponse($data): JsonResponse
+    {
+        $response = new Std200ListResponse();
+        $response->setItems($data);
+        $response->setStatusCode(HttpStatusCode::HTTP_OK);
+
+        return $this->response($response);
+    }
+
+    /**
+     * A helper method for success that returns a single item
+     *
+     * @param mixed $data
+     * @return JsonResponse
+     */
+    public function successResponse($data): JsonResponse
+    {
+        $response = new Std200Response();
+        $response->setItem($data);
+        $response->setStatusCode(HttpStatusCode::HTTP_OK);
+
+        return $this->response($response);
+    }
+
+    /**
+     * A helper method for success that returns empty response
+     *
+     * @return JsonResponse
+     */
+    public function successEmptyResponse(): JsonResponse
+    {
+        $response = new StdEmptyResponse();
+        $response->setStatusCode(HttpStatusCode::HTTP_OK);
+
+        return $this->response($response);
+    }
+}
