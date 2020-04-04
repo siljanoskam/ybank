73c4146839fe5e29d647432a356bc1f4fe08a8b1
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2020-04-04 12:22:09 +0200

    Update accounts table seeder with user_id

--- api/database/factories/AccountFactory.php
+++ api/database/factories/AccountFactory.php
@@ -19,6 +19,7 @@ use Faker\Generator as Faker;
 $factory->define(Account::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'balance' => $faker->numberBetween(20000, 50000)
+        'balance' => $faker->numberBetween(20000, 50000),
+        'user_id' => $faker->numberBetween(1, 5)
     ];
 });
